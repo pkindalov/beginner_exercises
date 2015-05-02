665cca7a371ef64f9b98985e6bbe931f87a2abe6
pkindalov <panayotkindalov@gmail.com>
2015-05-03 02:13:10 +0300

    Added Problem 14.* Current Date and Time

---
+++ Intro-Programming-Homework/14.CurrentDateAndTime/CurrentDateAndTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class CurrentDateAndTime
+    {
+        static void Main()
+        {
+            //Create a console application that prints the current date and time. Find out how in Internet.
+
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+
