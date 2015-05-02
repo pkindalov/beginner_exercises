626a628a854536b2bc11a94addae2ab36369575a
pkindalov <panayotkindalov@gmail.com>
2015-05-02 23:22:36 +0300

    Added definition and description of problem

--- Intro-Programming-Homework/HelloCSharp/HelloCSharp.cs
+++ Intro-Programming-Homework/HelloCSharp/HelloCSharp.cs
@@ -4,6 +4,11 @@
     {
         static void Main()
         {
+            //Problem 4. Hello World
+            //Create, compile and run a “Hello C#” console application.
+            //Ensure you have named the application well (e.g. “”HelloCSharp”).
+            //You should submit the Visual Studio project holding the HelloCSharp class as part of your homework
+
             Console.WriteLine("Hello C#");
         }
     }
