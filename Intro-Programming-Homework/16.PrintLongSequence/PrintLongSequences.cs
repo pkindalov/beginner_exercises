e1eb5c6361aff59efc9f5fe1029574c1ee1e2ec2
pkindalov <panayotkindalov@gmail.com>
2015-05-03 12:33:40 +0300

    Problem 16.* Print Long Sequence

---
+++ Intro-Programming-Homework/16.PrintLongSequence/PrintLongSequences.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+    class PrintLongSequences
+    {
+        static void Main()
+        {
+            //Write a program that prints the first 1000 members of the sequence: 2, -3, 4, -5, 6, -7, …
+
+            int number;
+
+            for (int sequences = 2; sequences <= 1001; sequences++)
+            {
+                if (sequences % 2 == 0)
+                {
+                    number = sequences;
+                }
+                else
+                {
+                    number = sequences * (-1);
+                }
+                Console.WriteLine(number);
+            }
+        }
+    }
+
