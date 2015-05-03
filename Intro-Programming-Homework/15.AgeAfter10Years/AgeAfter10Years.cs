6120095e36532645f02b1ed335572d5d0b7fb149
pkindalov <panayotkindalov@gmail.com>
2015-05-03 12:03:36 +0300

    Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
    I had added a little more interactivity. The console application ask you to write years in future, what you want, not limited to 10 years.

---
+++ Intro-Programming-Homework/15.AgeAfter10Years/AgeAfter10Years.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    class AgeAfter10Years
+    {
+        static void Main()
+        {
+            //Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+
+            Console.WriteLine("Write your birth year: ");
+            int yourBirthYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your age after how many years: ");
+            int afterHowManyYears = int.Parse(Console.ReadLine());
+      
+            DateTime dateNow = new DateTime(2015, 5, 3);
+
+            int yourAgeNow = dateNow.Year - yourBirthYear;
+            Console.WriteLine("You are now at age " +yourAgeNow);
+            Console.WriteLine("After " + afterHowManyYears + " years" + " you will be at " + (yourAgeNow + afterHowManyYears));
+           
+        }
+    }
+
