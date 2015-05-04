4bfb4aba97d9a8196dd8cd3c68def09a3a82e114
pkindalov <panayotkindalov@gmail.com>
2015-05-04 20:23:59 +0300

    Problem 5. Boolean Variable

---
+++ PrimitiveDataTypes-Homework/5. BooleanVariable/BooleanVariable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+    class BooleanVariable
+    {
+        static void Main()
+        {
+           // Declare a Boolean variable called isFemale and assign an appropriate value corresponding to your gender.
+           //Print it on the console.
+
+            bool isFemale = false;
+            Console.WriteLine("Am I a female ? {0}", isFemale);
+        }
+    }
+
