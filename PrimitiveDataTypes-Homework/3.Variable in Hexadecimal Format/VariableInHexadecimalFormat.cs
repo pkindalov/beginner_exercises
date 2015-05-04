06277d7cd8340af0398e33f94671e56ee1474e20
pkindalov <panayotkindalov@gmail.com>
2015-05-04 20:00:41 +0300

    Variable in Hexadecimal Format

---
+++ PrimitiveDataTypes-Homework/3.Variable in Hexadecimal Format/VariableInHexadecimalFormat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class VariableInHexadecimalFormat
+    {
+        static void Main()
+        {
+            int someNumber = 0xFE; //FE - 254 in hex
+
+            Console.WriteLine(someNumber);
+        }
+    }
+
