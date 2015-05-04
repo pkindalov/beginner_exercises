3ac3184b13e0e37da6632c2e0219af158931e659
pkindalov <panayotkindalov@gmail.com>
2015-05-04 20:08:03 +0300

    Unicode Character

---
+++ PrimitiveDataTypes-Homework/4.UnicodeCharacter/UnicodeCharacter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class UnicodeCharacter
+    {
+        static void Main()
+        {
+            char star = '\u002A';
+
+            Console.WriteLine(star);
+        }
+    }
+
