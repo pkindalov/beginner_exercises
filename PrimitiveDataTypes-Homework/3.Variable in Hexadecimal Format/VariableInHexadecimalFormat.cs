420b6d0517cbc9c136b29c405bce095a4c7298eb
pkindalov <panayotkindalov@gmail.com>
2015-05-04 20:13:58 +0300

    Added Description of problem in comment lines

--- PrimitiveDataTypes-Homework/3.Variable in Hexadecimal Format/VariableInHexadecimalFormat.cs
+++ PrimitiveDataTypes-Homework/3.Variable in Hexadecimal Format/VariableInHexadecimalFormat.cs
@@ -4,6 +4,11 @@
     {
         static void Main()
         {
+            //Declare an integer variable and assign it with the value 254 in hexadecimal format (0x##).
+            //Use Windows Calculator to find its hexadecimal representation.
+            //Print the variable and ensure that the result is 254.
+
+
             int someNumber = 0xFE; //FE - 254 in hex
 
             Console.WriteLine(someNumber);
