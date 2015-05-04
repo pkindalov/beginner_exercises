9aa5294ba289a5668df66ce5131b10d2e7d35154
pkindalov <panayotkindalov@gmail.com>
2015-05-04 18:47:26 +0300

    Primitive Data Types Solution for Homework

---
+++ PrimitiveDataTypes-Homework/1.Declare Variables/DeclareVariables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class DeclareVariables
+    {
+        static void Main()
+        {
+            ushort numberOne = 52130;
+            sbyte numberTwo = -115;
+            int numberTree = 4825932;
+            sbyte numberFour = 97;
+            short numberFive = -10000;
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}", numberOne, numberTwo, numberTree, numberFour, numberFive);
+        }
+    }
+
