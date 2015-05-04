f9135222f3f1925d1d8f5d4efbd514de55176006
pkindalov <panayotkindalov@gmail.com>
2015-05-04 20:16:24 +0300

    Added Description of problem in comment lines

--- PrimitiveDataTypes-Homework/2FloatOrDouble/FloatOrDouble.cs
+++ PrimitiveDataTypes-Homework/2FloatOrDouble/FloatOrDouble.cs
@@ -4,6 +4,12 @@
     {
         static void Main()
         {
+           // Which of the following values can be assigned to a variable of type float 
+           //and which to a variable of type double: 34.567839023, 12.345, 8923.1234857, 3456.091?
+           //Write a program to assign the numbers in variables and print them to ensure no precision is lost.
+
+
+
             double numberOne = 34.567839023;
             float numberTwo = 12.345F;
             double numberThree = 8923.1234857;
