dca0c5e2310bf865e03643a843f694b137d80691
pkindalov <panayotkindalov@gmail.com>
2015-05-04 20:11:34 +0300

    Added Description of problem in comment lines

--- PrimitiveDataTypes-Homework/4.UnicodeCharacter/UnicodeCharacter.cs
+++ PrimitiveDataTypes-Homework/4.UnicodeCharacter/UnicodeCharacter.cs
@@ -4,6 +4,10 @@
     {
         static void Main()
         {
+            //Declare a character variable and assign it with the symbol that has Unicode code 42 (decimal) using 
+            //the \u00XX syntax, and then print it.
+
+
             char star = '\u002A';
 
             Console.WriteLine(star);
