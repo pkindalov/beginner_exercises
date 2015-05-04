f7dcac03cbff026d22d2374063ec48929ecd9f0a
pkindalov <panayotkindalov@gmail.com>
2015-05-04 19:31:45 +0300

    Added Problem 2. Float or Double?

---
+++ PrimitiveDataTypes-Homework/2FloatOrDouble/FloatOrDouble.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    class FloatOrDouble
+    {
+        static void Main()
+        {
+            double numberOne = 34.567839023;
+            float numberTwo = 12.345F;
+            double numberThree = 8923.1234857;
+            float numberFour = 3456.091F;
+
+            Console.WriteLine("{0}, {1}, {2}, {3}", numberOne, numberTwo, numberThree, numberFour);
+        }
+    }
+
