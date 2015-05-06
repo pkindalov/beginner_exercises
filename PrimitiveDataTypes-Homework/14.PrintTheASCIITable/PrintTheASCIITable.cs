5813a1f9d329f4a90f990b5484b071156e8d51b5
pkindalov <panayotkindalov@gmail.com>
2015-05-06 19:36:29 +0300

    Problem 14.* Print the ASCII Table

---
+++ PrimitiveDataTypes-Homework/14.PrintTheASCIITable/PrintTheASCIITable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+    class PrintTheASCIITable
+    {
+        static void Main()
+        {
+
+            for (int c = 0; c <= 128; c++)
+            {
+                Console.Write("{0}", (char)c);
+            }
+
+        }
+    }
+
