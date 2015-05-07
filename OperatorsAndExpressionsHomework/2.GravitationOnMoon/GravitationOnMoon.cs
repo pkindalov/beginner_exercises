478a580548e1381334400eace57713d6b976a8a0
pkindalov <panayotkindalov@gmail.com>
2015-05-07 11:15:03 +0300

    Problem 2. Gravitation on the Moon

---
+++ OperatorsAndExpressionsHomework/2.GravitationOnMoon/GravitationOnMoon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+    class GravitationOnMoon
+    {
+        static void Main()
+        {
+            //The gravitational field of the Moon is approximately 17% of that on the Earth.
+            //Write a program that calculates the weight of a man on the moon by a given weight on the Earth.
+
+            Console.WriteLine("Enter your Earth weight: ");
+            double yourEarthWeight = double.Parse(Console.ReadLine());
+
+            yourEarthWeight *= 0.17;
+            Console.WriteLine("Your weight on Moon will be {0}", yourEarthWeight);
+        }
+    }
+
