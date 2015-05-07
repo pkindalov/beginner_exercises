4121175ef995650b598658120993c0784731a1b9
pkindalov <panayotkindalov@gmail.com>
2015-05-07 10:59:39 +0300

    Operators and expressions homeworks

---
+++ OperatorsAndExpressionsHomework/1.OddOrEvenIntegers/OddOrEvenIntegers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+    class OddOrEvenIntegers
+    {
+        static void Main(string[] args)
+        {
+
+             bool isOdd = false;
+            Console.WriteLine("Enter some number: ");
+            int someNumber = int.Parse(Console.ReadLine());
+
+            if ((someNumber % 2) == 0)
+            {
+                isOdd = false;
+            }
+            else
+            {
+                isOdd = true;
+            }
+
+            Console.WriteLine(isOdd);
+
+        }
+
+        }
+    
+
