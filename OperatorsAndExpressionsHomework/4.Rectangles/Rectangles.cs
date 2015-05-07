2ae9950411f8e1e6b48022738f6540f9a93ff626
pkindalov <panayotkindalov@gmail.com>
2015-05-07 12:00:12 +0300

    Problem 4. Rectangles

---
+++ OperatorsAndExpressionsHomework/4.Rectangles/Rectangles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Rectangles
+    {
+        static void Main()
+        {
+            //Write an expression that calculates rectangle’s perimeter and area by given width and height.
+
+            Console.WriteLine("Enter width of rectangle: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Height of rectangle: ");
+            double height = double.Parse(Console.ReadLine());
+
+            double perimeterRectangle = (width + height) * 2;
+            double areaRectangle = width * height;
+
+            Console.WriteLine("Perimeter of rectangle: {0} Area: {1}", perimeterRectangle, areaRectangle);
+
+        }
+    }
