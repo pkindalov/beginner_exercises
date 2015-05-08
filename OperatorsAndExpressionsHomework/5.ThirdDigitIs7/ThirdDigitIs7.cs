86b4e71193e2158d4438b414f652242ddb78b546
pkindalov <panayotkindalov@gmail.com>
2015-05-08 09:31:35 +0300

    Problem 5. Third Digit is 7?

---
+++ OperatorsAndExpressionsHomework/5.ThirdDigitIs7/ThirdDigitIs7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+    class ThirdDigitIs7
+    {
+        static void Main()
+        {
+            //Write an expression that checks for given integer if its third digit from right-to-left is 7
+
+            Console.WriteLine("Enter a number: ");
+            int yourNumber = int.Parse(Console.ReadLine());
+
+            bool checkFor7 = ((yourNumber / 100) % 10 == 7);
+            Console.WriteLine(checkFor7);
+
+        }
+    }
+
