afc95a70ddee3bf27f3b86436f9bd8ee5ce2dd58
pkindalov <panayotkindalov@gmail.com>
2015-05-20 12:51:04 +0300

    ConsoleInputOutput homeworks

---
+++ ConsoleInputOutput - Homework/1.SumOf3Numbers/SumOf3Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+
+
+    class SumOf3Numbers
+    {
+        static void Main()
+        {
+            //Write a program that reads 3 real numbers from the console and prints their sum.
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;  //set regional settings to universal with purpose console to not
+                                                                                //broke down when we entered symbols - ',' or '.' depends of environment
+                                                                                 
+            Console.WriteLine("Enter first number: ");
+            double userNumber1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            double userNumber2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a third number: ");
+            double userNumber3 = double.Parse(Console.ReadLine());
+
+            double sum = (double)(userNumber1 + userNumber2 + userNumber3);
+
+            Console.WriteLine("The sum of numbers is: {0}", sum);
+
+        }
+    }
+
