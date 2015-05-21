3dcc6e1735467c6ee557b538550c424e02d80c33
pkindalov <panayotkindalov@gmail.com>
2015-05-21 14:09:23 +0300

    Problem 3. Circle Perimeter and Area

---
+++ ConsoleInputOutput - Homework/3.CirclePerimeterAndArea/CirclePerimeterAndArea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+
+
+
+    class CirclePerimeterAndArea
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.Write("Enter radius: ");
+            double r = double.Parse(Console.ReadLine());
+            Console.Write("Enter a perimeter: ");
+            double perimeter = double.Parse(Console.ReadLine());
+
+            double area = (r * perimeter) / 2;
+
+            Console.Write("Perimeter: {0:F2}, \n Area: {1:F2}\n", perimeter, area);
+        }
+    }
+
