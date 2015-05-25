3ef6affea7ad5b4f2a6fb861af71be9172338c18
pkindalov <panayotkindalov@gmail.com>
2015-05-25 10:38:33 +0300

    Problem 10. Fibonacci Numbers

---
+++ ConsoleInputOutput - Homework/10.FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+    class FibonacciNumbers
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            BigInteger f0 = 0;
+            BigInteger f1 = 1;
+            BigInteger sum;
+
+            Console.WriteLine(f1);
+
+            for(int i = 0; i <= number; i++)
+            {
+                sum = f0 + f1;
+                f0 = f1;
+                f1 = sum;
+
+                Console.WriteLine(sum);
+            }
+
+
+
+
+
+        }
+    }
+
