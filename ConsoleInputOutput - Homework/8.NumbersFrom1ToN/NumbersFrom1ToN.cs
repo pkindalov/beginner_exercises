04bc022443ad95177018711187cac88be783b9f2
pkindalov <panayotkindalov@gmail.com>
2015-05-25 10:23:22 +0300

    Problem 8. Numbers from 1 to n

---
+++ ConsoleInputOutput - Homework/8.NumbersFrom1ToN/NumbersFrom1ToN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    class NumbersFrom1ToN
+    {
+        static void Main()
+        {
+            //Write a program that reads an integer number n from the console and prints all the numbers in the interval [1..n], each on a single line.
+
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+
