3961209734dce9f561c90701e36367c7547db632
pkindalov <panayotkindalov@gmail.com>
2015-05-25 10:24:44 +0300

    Problem 9. Sum of n Numbers

---
+++ ConsoleInputOutput - Homework/9.SumOfN Numbers/SumOfN Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+
+    class Program
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.Write("Enter number: ");
+            double number = double.Parse(Console.ReadLine());
+            double sum = 0;
+
+            for (double i = 1; i <= number; i++)
+            {
+
+                Console.Write("Enter number {0}: ", i);
+                double n = double.Parse(Console.ReadLine());
+                sum += n;
+
+
+            }
+
+            Console.WriteLine(sum);
+
+
+        }
+    }
+
